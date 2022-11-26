91c66ee0a191bbfc910e6792b3183544c17b0a3b
thomas-lerchbaumer-fh <71784753+thomas-lerchbaumer-fh@users.noreply.github.com>
2022-11-26 22:16:40 +0100

    *inital commit

---
+++ src/context/types.js
@@ -0,0 +1,6 @@
+export const USER_LOADED = 'USER_LOADED';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const LOGIN_SUCCESS = 'LOGIN_SUCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+export const CLEAR_ERRORS = 'CLEAER_ERRORS';
